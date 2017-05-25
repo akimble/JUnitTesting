77df7a489c7fce89ef1480c1428e77d4641e2836
akimble <kimble00550117@gmail.com>
2017-05-25 00:45:35 -0400

    Removed public modifiers. Created appendAndSort method and JUnit test

--- JUnitTest/src/testing/AllTests.java
+++ JUnitTest/src/testing/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ countATest.class, squareTest.class })
+@SuiteClasses({ countATest.class, squareTest.class, appendAndSortTest.class })
 public class AllTests {
 
 }
--- JUnitTest/src/testing/JunitTesting.java
+++ JUnitTest/src/testing/JunitTesting.java
@@ -1,18 +1,29 @@
 package testing;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class JunitTesting {
+
+	//No need for public methods right now
+	int square(int x){
+		return x*x;
+	}
+
+	int countA(String word){
+		int count = 0;
+		for(int i=0;i<word.length();i++){
+			if(word.charAt(i)=='a' || word.charAt(i)=='A'){
+				count++;
+			}
+		}
+		return count;
+	}
 	
-  public int square(int x){
-	  return x*x;
-  }
-  
-  public int countA(String word){
-	  int count = 0;
-	  for(int i=0;i<word.length();i++){
-		  if(word.charAt(i)=='a' || word.charAt(i)=='A'){
-			  count++;
-		  }
-	  }
-	  return count;
-  }
+	ArrayList<String> appendAndSort(ArrayList<String> wordArray1, ArrayList<String> wordArray2){
+		wordArray1.removeAll(wordArray2); //remove duplicates
+		wordArray1.addAll(wordArray2); //merge into wordArray1
+		Collections.sort(wordArray1);
+		return wordArray1;
+	}
 }
